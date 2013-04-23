ebe8974eec419ac338b1f4345a7ec3e026151d7d
liorur <liorur@gmail.com>
2013-04-23 12:39:20 +0300

    Update install_laravel4.sh

--- install_laravel4.sh
+++ install_laravel4.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
-#   Download and install Laravel 4 ( with Generator Guard and Profiler )
+#   This script will download and install Laravel 4 ( with Generator Guard and Profiler ) 
+# 
+#   platform: UNIX
 #  
 #   Written by Natan Shalva   20.4.2013 
 #    
@@ -9,11 +11,11 @@
 # install laravl 4 
 
 
-printf "\n \n Welcome, We are going to download and install Laravel 4. \n \n";
+printf "\n \n Welcome, You are about to download and install Laravel 4. \n \n";
 
 cd /var/www/
 
-printf "Please write your new Laravel 4 site project directory \n\r ( it will be create in /var/www/ )\n\r"
+printf "Please type in directory name for your new Laravel 4 website project  \n\r ( the dir will be created under /var/www/ )\n\r"
 read _dir
 if [ -d /var/www/$_dir ] ; then
 
@@ -31,12 +33,11 @@ printf "\n \n Creating new dir name: $_dir \n \n\r"
 cd /var/www/
 mkdir $_dir
 
-printf "\n Give the $_dir 777 permissions \n\r "
+printf "\n Assigning $_dir 777 permissions \n\r "
 chmod -R 777 $_dir
 
 cd $_dir; 
 
-printf "\n we are in the dir "
 pwd ;
 
 printf "\n install composer \n"
@@ -52,45 +53,43 @@ php composer.phar install
 
 _laravel4="https://github.com/laravel/laravel/archive/develop.zip"
 
-printf "\n We will download Laravl 4 from: $_laravel4 \n Is this good for you ? (y/n)\n\r"
+printf "\n Fetching Laravl 4 from: $_laravel4 \n please select y to proceed n to select a different source: (y/n)\n\r"
 
 read answer
 if [ $answer = n ] ; then
-    printf "\n Please insert download link to laravl 4 "
+    printf "\n Please type download link for laravl 4 "
     read _laravel4
 fi 
 
-printf "\n Downloading Laravel 4 and unzip \n";
+printf "\n Downloading and unzipping Laravel 4\n";
 wget $_laravel4 ;
 unzip '*.zip' ;
 
 
-printf "\n Give the $_dir 777 permissions \n " ;
+printf "\n Assigning $_dir 777 permissions \n " ;
 chmod -R 777 ../$_dir ;
 
 
-printf "\n Move the unzip up one level \n " ;
 #cd laravel-develop ;
 pwd ;
 rsync -avz laravel-develop/ ./
 printf "\n we are in: " ;
 pwd
-printf "\n Just to test if all good - let's see the list of files: \n" ;
+printf "\n Verifying installation  -  file list: \n" ;
 ls -la;
 
-printf "\n Remove zip and laravel-develop folder \n"
+printf "\n Removing zip and laravel-develop folder \n"
 rm -rf laravel-develop develop.zip ;
 
-printf "\n Execute composer update \n"
+printf "\n Running composer update \n"
 if [ -f ./composer.json ] ; then 
 		composer update
 else 
-	printf "\n Opps, can't find composer.json file \n";
+	printf "\n Oops, can't find composer.json \n";
 	# exit the sctipt 
 	return;
 fi	
 
-printf "\n We are in the directory: ";
 pwd ;
 
 	   
@@ -110,34 +109,34 @@ printf "\n \n Create database with the same name as the file directory (y/n) \n\
 read answer
 if [ $answer = y ] ; then
 
-    printf "\n Enter your mysql user \n\r"
+    printf "\n Enter mysql user name \n\r"
     read _user
 
-    printf "\n Enter your mysql user password \n\r"
+    printf "\n Enter mysql password \n\r"
     read _pass
 
     if [ -d /var/lib/mysql/$_dir ] ; then
 
-        printf "\n Database exist, whould you like to delete it and create new one ? (y/n) \n\r"
+        printf "\n Database already exists, whould you like to delete it and create new one ? (y/n) \n\r"
         read answer
         if [ $answer = y ] ; then
         	mysqladmin -u $_user -p"$_pass" drop $_dir ;
-        	printf "\n database deleted \n"
-        	printf "\n Create the new database $_dir \n "
+        	printf "\n Deleting database \n"
+        	printf "\n Creating new database $_dir \n "
         	mysqladmin -u $_user -p"$_pass" create $_dir
         fi
 
     else
-        printf "\n Create the database $_dir \n "
+        printf "\n Creating database $_dir \n "
         mysqladmin -u $_user -p"$_pass" create $_dir
     fi
 
-    printf  "\n \n ---------- Good, we finish with the database issues  --------------- \n "
+    printf  "\n \n ---------- Database installation complete --------------- \n "
 
 
 
 
-printf "\n Update the database name and the user in Laravl 4 config ./app/config/database.php"
+printf "\n Configuring database name and the user in Laravl 4 config ./app/config/database.php"
 
 printf "\n create ./app/config/database.php.orig file \n "
 mv ./app/config/database.php ./app/config/database.php.orig ;
@@ -148,7 +147,7 @@ mv ./app/config/database.php ./app/config/database.php.orig ;
 
 
 
-printf "\n Install migrate \n "
+printf "\n Installing migrate \n "
 eval "php artisan migrate:install"
 
 fi 
@@ -173,17 +172,17 @@ printf "\n would you like to install the following packages ? \n\n
 read answer
 if [ $answer = y ] ; then
 
-    printf "\n Update the Generator Profiler Guard to the file composer.json \n "
+    printf "\n Configure Generator Profiler Guard in composer.json \n "
     mv ./composer.json ./composer.json.orig
 
     sed "s|\"laravel/framework\": \"4.0.*\"|\"laravel/framework\": \"4.0.*\",|" ./composer.json.orig > ./composer.json.orig2
 
     sed "4 i\ \"way/generators\": \"dev-master\", \n        \"loic-sharma/profiler\": \"1.0.*\",\n        \"way/guard-laravel\": \"dev-master\"  "  ./composer.json.orig2 > ./composer.json
 
-    printf "\n run composer update \n "
+    printf "\n Run composer update \n "
     composer update
 
-    printf "\n Update the Generator Profiler Guard to the file /app/config/app.php \n "
+    printf "\n Configure Generator Profiler Guard in /app/config/app.php \n "
 
     printf "\n create ./app/config/app.php.orig file \n"
     mv ./app/config/app.php ./app/config/app.php.orig ;
@@ -195,27 +194,26 @@ if [ $answer = y ] ; then
     
     rm ./app/config/app.php.orig2 ;
 
-    printf "\n Give the $_dir 777 permissions \n " ;
+    printf "\n Assign $_dir 777 permissions \n " ;
     chmod -R 777 ../$_dir ;
 
 
-    printf  "\n \n---------- Good, we finish config the files to work with Generator, Profiler, Guard.   --------------- \n "
+    printf  "\n \n---------- Generator, Profiler and Guard installation complete   --------------- \n "
 
-    printf "\n Would you like to create info file on your project \n
-with the packages info for more instructions how to use them ? (y/n) \n\r"
+    printf "\n Would you like to create packages info and instruction file ? (y/n) \n\r"
     read answer
     if [ $answer = y ] ; then
 
     printf  "\n\n Generator - by the one and the only Jeffry way \n\n
-         You can see it in gitHub: https://github.com/JeffreyWay/Laravel-4-Generators
+         gitHub: https://github.com/JeffreyWay/Laravel-4-Generators
 
          Guard - by the one and the only Jeffry way \n\n
-         You can see it in gitHub: https://github.com/JeffreyWay/Laravel-Guard
+         gitHub gitHub: https://github.com/JeffreyWay/Laravel-Guard
 
          The Profiler - written by Loic Sharma \n\n\
-         You can see it in gitHub: https://github.com/loic-sharma/profiler \n\n\ " > packages.info
+         gitHub: https://github.com/loic-sharma/profiler \n\n\ " > packages.info
     
-        printf  "\n New file create are you root project path name: packages.info"
+        printf  "\n packages.info created at project root directory"
 
     fi
 fi 
@@ -225,30 +223,30 @@ fi
 printf "\n \n install git \n "
 git init
 
-printf "\n \n add all files to git and commit them \n "
+printf "\n \n Adding and commiting project to git  \n "
 git add -A
 git commit -am"Good Start"
 
-printf "\n \n We have commit all the files \n "
+printf "\n \n Project commited \n "
 
 
 # virtual host
 
-printf "\n \n \n whould you like to create VirtualHost config to Laravl 4 ? (y/n) \n\r"
+printf "\n \n \n Would you like to create VirtualHost config for Laravl 4 ? (y/n) \n\r"
 read answer
 
 if [ $answer = y ] ; then
 
 virtualhost=y
 
-    printf "\n create virtual host \n "
+    printf "\n Creating virtual host \n "
 
-    printf "\n Enter the user (if you don't know what to write here the it is probably root ) \n\r"
+    printf "\n Type in user (if you don't know what to write here the it is probably root ) \n\r"
     read usr
 
 homedir=$_dir ;
 
-    printf "\n Enter domain ( exp: laravl.local ) \n\r "
+    printf "\n Type in domain ( exp: laravl.local ) \n\r "
     read sn
 
 
@@ -256,7 +254,7 @@ homedir=$_dir ;
     mkdir /var/log/apache2/$sn/
 
 
-    # Creation the file with VirtualHost configuration in /etc/apache2/site-available/
+    # Create  VirtualHost configuration file in /etc/apache2/site-available/
     printf "<VirtualHost *:80>
             ServerAdmin webmaster@$sn
             ServerName $sn
@@ -294,11 +292,11 @@ homedir=$_dir ;
         printf "the vhost is allready in /etc/hosts "
 
     else 
-        printf "i am adding $sh to /etc/hosts "  
+        printf "Adding $sh to /etc/hosts "  
         printf "127.0.0.1 $sn" >> /etc/hosts
     fi
 
-    # Enable the site 
+    # Enable site 
     a2ensite $sn
 
     # Reload Apache2
@@ -313,7 +311,7 @@ printf "\n \n"
 printf "\n \n ************ All GOOD! **************  \n \n" ;
 
 if [ $virtualhost = y ] ; then
-printf "\n \n You can see you new Laravel at:  \n http://$_dir  \n \n" ;
+printf "\n \n New Laravel project can be viewed at:  \n http://$_dir  \n \n" ;
 fi
 
 printf "\n \n Script written by Natan Shalva \n to promote peace love and understanding \n \n" 
